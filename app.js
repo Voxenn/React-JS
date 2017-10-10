dd2f3a303939bef3a7c553c5da4a02bc756bebc0
Vernon Williams <vwill035@fiu.edu>
2017-10-09 22:45:17 -0500

    Initial commit.

---
+++ app.js
@@ -0,0 +1,25 @@
+//React.JS learning
+//Create a simple car website!
+const make = (
+  <div>
+    <h1>
+      Honda
+    </h1>
+  </div>
+);
+
+const model = (
+  <div>
+    <h1>
+      Civic
+    </h1>
+  </div>
+);
+
+const color = (
+  <div>
+    <h1>
+      Black
+    </h1>
+  </div>
+);
